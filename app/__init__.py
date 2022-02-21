930580ff84c7ede45d27183ec3904a5c0cb12d6b
Makokha Victor <marcosvictos@gmail.com>
2022-02-21 15:29:09 +0300

    Fix app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .services import celery
-from .model import db
-
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    with app.app_context():
-
-
