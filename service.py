e718733c7a3ef714bb9bd5c65dfbe8a5a177038d
Gidraf Orenja <gidraforenja@outlook.com>
2022-03-30 08:45:57 +0300

    add action on notification when clicked

--- service.py
+++ service.py
@@ -16,7 +16,7 @@ logger = get_task_logger(__name__)
 def get_article(url,read_time):
     # logger.error("I am error")
     """Scrape URLs to generate article content."""
-    subprocess.call(f'''termux-vibrate -d 5000 -f && termux-notification  --sound --title "Time to read {url}" --id "{url}" --button1 "Read Now"  --priority "high"''', shell=True)
+    subprocess.call(f'''termux-vibrate -d 5000 -f && termux-notification  --sound --title "Time to read {url}" --id "{url}"  --action "xdg-open {url}"''', shell=True)
 
 
 def getImage(link):
