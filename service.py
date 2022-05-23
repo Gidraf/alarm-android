547ba263433e4efdd55e41f76e26a8e9653e9d3f
Gidraf Orenja <gidraforenja@outlook.com>
2022-05-23 15:19:51 +0300

    add description

--- service.py
+++ service.py
@@ -1,10 +1,11 @@
 from celery import Celery
 import subprocess
-
+import requests
 celery = Celery("lma_scheduler", broker="redis://127.0.0.1:6379/0")
 
 @celery.task
 def get_article(url,read_time):
     # logger.error("I am error")
     """Scrape URLs to generate article content."""
-    subprocess.call(f'''termux-vibrate -d 5000 -f && termux-notification  --sound --title "Time to read {url}" --id "{url}"  --action "xdg-open {url}"''', shell=True)
+    text = requests.get(url).text
+    subprocess.call(f'''termux-vibrate -d 5000 -f && termux-notification  --sound --title "Time to read {url}" --id "{url}"  --action "xdg-open {url}" --content{"Desciption here"}''', shell=True)
