31d47139bfe93340238845f85f09f91af8d03e1b
Gidraf Orenja <gidraforenja@outlook.com>
2022-05-23 16:35:50 +0300

    add arrow

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def home():
         url = request.form.get("url")
         time = request.form.get("time")
         # import pdb; pdb.set_trace()
-        eta = arrow.get(time,"Africa/Nairobi").utcnow()
+        eta = arrow.get(time).utcnow()
         get_article.apply_async(args=[url, time], eta=eta.to('Africa/Nairobi'))
         return render_template("success.html",sucess="get"), 200
 
